5d2e85ada12831e24f44a784b4c2024e86c3c54d
Petnir-Sauwa-Shei-Hot <144002448+Petnir-Sauwa-Shei-Hot@users.noreply.github.com>
2023-09-04 14:09:32 +0600

    Add files via upload

---
+++ pkg.sh
@@ -0,0 +1,35 @@
+echo "
+
+
+ 
+ .----------------.  .----------------. 
+| .--------------. || .--------------. |
+| |      __      | || |  _______     | |
+| |     /  \     | || | |_   __ \    | |
+| |    / /\ \    | || |   | |__) |   | |
+| |   / ____ \   | || |   |  __ /    | |
+| | _/ /    \ \_ | || |  _| |  \ \_  | |
+| ||____|  |____|| || | |____| |___| | |
+| |              | || |              | |
+| '--------------' || '--------------' |
+ '----------------'  '----------------' 
+
+                
+
+"
+echo "======================================="
+echo "   Tools Name : Basic Pkg Install"
+echo "   Author : AR To Rafi"
+echo "   Fb Group : Team STLP"
+echo "=======================================19 Aug 2023...Time:5.05 Pm"
+termux-setup-storage
+echo " Storage Permission Granted"
+pkg update
+pkg upgrade
+pkg install python -y
+echo "Python Installed Successfully"
+pkg install python2 -y
+echo ""
+echo ""
+echo "Love You M "
+echo "All Package Installed"
